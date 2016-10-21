ed847e6be475b68314c24aeb02886d5ecfc36d46
Eazuara <A01410412@itesm.mx>
2016-10-20 19:49:40 -0500

    Update FPP9.java

--- FP-P9/src/fp/p9/FPP9.java
+++ FP-P9/src/fp/p9/FPP9.java
@@ -16,22 +16,22 @@ public class FPP9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        // Declaracion de variables
 int num;
 num=pedirNumero();
 Desgloce(num);
 
 }
-public static int pedirNumero(){
+public static int pedirNumero(){//inGRESAR Numero a Desglosar
 Scanner entrada = new Scanner(System.in);
 System.out.print("Ingrese el numero para Desglosar ");
 int num=entrada.nextInt();
 return num;
 }
-public static void Desgloce(int n){
-System.out.print(n+" = ");
-for(int i=10;i<n*10;i=i*10){
-System.out.print(((n%i)/(i/10))*(i/10));
+public static void Desgloce(int n){//metodo para desglosar
+System.out.print(n+" = ");//Numero es igual 
+for(int i=10;i<n*10;i=i*10){//Condicionar
+System.out.print(((n%i)/(i/10))*(i/10));//Imprimir
 if ((i<n))System.out.print(" + ");
 }
 System.out.println("");
